10b0661be9ab02028f7e95e3468f160aa97b9e6a
stdAlexTikhonov <std1992@mail.ru>
2021-01-30 00:18:48 +0300

    Added prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-
-ReactDOM.render(
-  <div>Hello</div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<div>Hello</div>, document.getElementById("root"));
