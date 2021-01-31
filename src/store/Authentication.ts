defc645fce58259055ad078d5e4f31ef65e83567
stdAlexTikhonov <std1992@mail.ru>
2021-01-31 16:16:17 +0300

    TASK-6 Login Logout Works

--- src/store/Authentication.ts
+++ src/store/Authentication.ts
@@ -1,16 +1,36 @@
-import { AutorenewSharp } from "@material-ui/icons";
 import { makeAutoObservable } from "mobx";
 import { userLogin } from "../utils/api";
 
 class Auth {
   token: string | boolean = false;
+  login: string = "";
+  password: string = "";
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  async login(login: string, password: string) {
-    this.token = await userLogin(login, password);
+  async loginUser() {
+    const res = await userLogin(this.login, this.password);
+    if (res) this.token = res;
+    else {
+      this.login = "";
+      this.password = "";
+    }
+  }
+
+  setLogin(login: string) {
+    this.login = login;
+  }
+
+  setPassword(pass: string) {
+    this.password = pass;
+  }
+
+  logout() {
+    this.token = false;
+    this.login = "";
+    this.password = "";
   }
 }
 
